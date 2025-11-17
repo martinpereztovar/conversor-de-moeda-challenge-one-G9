c0fc9831fe36bc574a12cc617b240160ed1cb079
Martin <martinpereztovar@gmail.com>
2025-11-17 17:38:12 -0300

    Ajuste na API KEY - criando variavel de ambiente no IntelliJ

--- src/br/com/martinperez/conversor/config/Config.java
+++ src/br/com/martinperez/conversor/config/Config.java
@@ -2,6 +2,14 @@ package br.com.martinperez.conversor.config;
 
 public class Config {
     public static String getApiKey() {
-        return "38ae7c217e791670dc76b86e";
+        String apiKey = System.getenv("EXCHANGE_RATE_KEY");
+
+        if (apiKey == null || apiKey.isBlank()) {
+            throw new RuntimeException(
+                    "API key not configured. Set EXCHANGE_RATE_KEY in your Run Configuration."
+            );
+        }
+
+        return apiKey;
     }
 }
