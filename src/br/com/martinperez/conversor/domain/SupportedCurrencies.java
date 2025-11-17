e270e33c3cba0abe7a1f11acc7da3ee8af11efdb
Martin <martinpereztovar@gmail.com>
2025-11-17 12:48:36 -0300

    Lista de moedas adicionada - Passo 8

---
+++ src/br/com/martinperez/conversor/domain/SupportedCurrencies.java
@@ -0,0 +1,30 @@
+package br.com.martinperez.conversor.domain;
+
+public class SupportedCurrencies {
+
+    public static String[] LIST = {
+            "USD", // Estados Unidos
+            "EUR", // Euro
+            "BRL", // Brasil
+            "ARS", // Argentina
+            "MXN", // México
+            "COP", // Colômbia
+            "CLP", // Chile
+            "PEN", // Peru
+            "PYG", // Paraguai
+            "UYU", // Uruguai
+            "VES", // Venezuela
+            "BOB", // Bolívia
+            "CAD", // Canadá
+            "GBP", // Reino Unido
+            "JPY", // Japão
+            "CRC", // Costa Rica
+            "DOP", // Rep. Dominicana
+            "HNL", // Honduras
+            "NIO", // Nicarágua
+            "GTQ", // Guatemala
+            "PAB", // Panamá
+            "SRD", // Suriname
+            "GYD"  // Guiana
+    };
+}
